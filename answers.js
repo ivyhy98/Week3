c26595df3a3d2757972d146425ae1a9e9041ba12
ivyhy98 <ivanahy98@gmail.com>
2022-10-13 14:32:59 -0400

    fizzbuzz answered

--- answers.js
+++ answers.js
@@ -7,29 +7,44 @@
 // Easy Going
 ////////////////////////////////
 
-for(let i =1; i<=20; i++){
-    //console.log(i)
-}
+// for(let i =1; i<=20; i++){
+//     //console.log(i);
+// }
 
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
 //Write a for loop that will log only the even numbers in 0 through 200.
 
-for(let x = 0; x<=200; x++){
-    if(x % 2 === 0){
-        console.log(x)
-    }
-}
+// for(let x = 0; x<=200; x++){
+//     if(x % 2 === 0){
+//         //console.log(x);
+//     }
+// }
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+for(let i = 1; i <= 100; i++){
+    if(i % 15 === 0){
+        console.log('fizzbuzz');
+    } else if(i % 3 === 0){
+        console.log('fizz');
+    } else if(i % 5 === 0){
+        console.log('buzz');
+    } else{
+        console.log(i);
+    }
+}
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
 
 
 ////////////////////////////////
